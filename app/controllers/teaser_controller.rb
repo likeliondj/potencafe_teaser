2acc8c0941f68853683850dcae9bfb4359ba7560
Duhee Lee <likelion.dj@gmail.com>
2013-06-15 00:48:24 +0900

    what's wrong with javascript....?

--- app/controllers/teaser_controller.rb
+++ app/controllers/teaser_controller.rb
@@ -3,4 +3,13 @@ class TeaserController < ApplicationController
   def index
 
   end
+
+  def save_mail
+
+  new_mail = Mail.new
+  new_mail = params[:address]
+  new_mail.save
+
+  render  :status => 200
+  end
 end
